2db17d776144c1e61a7acde0760a068a150b71f7
Mikeliuty <mikeliuty@gmail.com>
2018-04-05 23:32:58 +1200

    Init
    Made the software init

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React,{Component} from 'react';
+import {render} from 'react-dom';
+import {connect} from 'react-redux';
+
+class Header extends React.Component{
+  render(){
+    return (
+    <div>
+      test
+    </div>
+    );
+  }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/Main';
+import Header from './components/header'
 
-// Render the main component into the dom
+class App extends React.Component {
+  render() {
+    return (
+    <Header />
+    );
+  }
+}
+
 ReactDOM.render(<App />, document.getElementById('app'));
